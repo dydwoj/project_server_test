905239f6877f9314f5c03690228052e05d839507
dydwoj <dydwoj@gmail.com>
2025-08-14 13:16:36 +0900

    sql 수정

--- soltech/project/insert-sql/03_employee_seed_test.sql
+++ soltech/project/insert-sql/03_employee_seed_test.sql
@@ -2,9 +2,9 @@ INSERT INTO employee (
     emp_no, e_name, e_gender, e_birthday, e_email, e_address, e_mobile, e_account,
     e_password, e_hiredate, e_salary, e_member_role, e_dept_no, e_job_no
 ) VALUES
-(1001, '정용재', 'MALE',   '1999-03-03', 'yongjae@soltech.co.kr', '서울특별시 영등포구', '010-9733-6605', '110-999-999999', '$2b$12$c4r7CTXre.KzmKBex.g5rumhuR7PTQMfdEV/4u.uxFYMOZeEb48gy', '2018-05-11', 213000000, 'EMPLOYEE', 201, 'AD_MGR'),
-(1002, '정동진', 'MALE',   '1997-03-03', 'dongjin@soltech.co.kr', '부산광역시 해운대구', '010-8213-1159', '110-999-999999', '$2b$12$tKHA1gmw2YkHdhjfBH4qt.JLchZarKxC3X4kYPhi9cOHYSt54fk/C', '2018-05-11', 210000000, 'EMPLOYEE', 201, 'AD_MGR'),
-(1003, '이용성', 'MALE',   '1993-10-25', 'yongsung@soltech.co.kr', '서울특별시 강서구', '010-7129-7318', '110-999-999999', '$2b$12$zxNS5wYYWBHelM1JrBVr..G1VWGnvfglgNoh3DpHWVNw/lQdkK8qG', '2018-05-11', 208000000, 'EMPLOYEE', 201, 'AD_MGR'),
-(1004, '장미경', 'FEMALE', '2002-07-31', 'miaOwO@soltech.co.kr', '경기도 성남시', '010-8276-4087', '110-999-999999', '$2b$12$FuJMgzYfOCCO16dvfPoGFesbRrzTuZUZkNYgSTqacJ9w7phnFA/F.', '2018-05-11', 210000000, 'EMPLOYEE', 201, 'AD_MGR'),
-(1005, '박병선', 'MALE',   '1999-10-31', 'byeoungsun@soltech.co.kr', '서울특별시 은평구', '010-4605-8459', '110-999-999999', '$2b$12$yzqmqMs1OWZRxaeo3Yyct.Q3fJsBNipYEV5p2k8Hw2yU6gqMNsf0m', '2018-05-11', 205000000, 'EMPLOYEE', 201, 'AD_MGR'),
-(1006, '신승찬', 'MALE',   '2005-07-31', 'seungchan@soltech.co.kr', '강원도 인제군', '010-5571-2417', '110-999-999999', '$2b$12$E/4lH1jWpuK84HE0LS9/kOZdCcMPR/8XmT/j/acWK3jo32.Gwd4kW', '2018-05-11', 200000000, 'EMPLOYEE', 201, 'AD_MGR');
+(1001, '정용재', 'MALE',   '1999-03-03', 'yongjae@soltech.co.kr', '서울특별시 영등포구', '010-9733-6605', '110-999-999999', '1111', '2018-05-11', 213000000, 'EMPLOYEE', 201, 'AD_MGR'),
+(1002, '정동진', 'MALE',   '1997-03-03', 'dongjin@soltech.co.kr', '부산광역시 해운대구', '010-8213-1159', '110-999-999999', '1111', '2018-05-11', 210000000, 'EMPLOYEE', 201, 'AD_MGR'),
+(1003, '이용성', 'MALE',   '1993-10-25', 'yongsung@soltech.co.kr', '서울특별시 강서구', '010-7129-7318', '110-999-999999', '1111', '2018-05-11', 208000000, 'EMPLOYEE', 201, 'AD_MGR'),
+(1004, '장미경', 'FEMALE', '2002-07-31', 'miaOwO@soltech.co.kr', '경기도 성남시', '010-8276-4087', '110-999-999999', '1111', '2018-05-11', 210000000, 'EMPLOYEE', 201, 'AD_MGR'),
+(1005, '박병선', 'MALE',   '1999-10-31', 'byeoungsun@soltech.co.kr', '서울특별시 은평구', '010-4605-8459', '110-999-999999', '1111', '2018-05-11', 205000000, 'EMPLOYEE', 201, 'AD_MGR'),
+(1006, '신승찬', 'MALE',   '2005-07-31', 'seungchan@soltech.co.kr', '강원도 인제군', '010-5571-2417', '110-999-999999', '1111', '2018-05-11', 200000000, 'EMPLOYEE', 201, 'AD_MGR');
